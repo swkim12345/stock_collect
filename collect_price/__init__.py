87085007196b098197b818898dc19dd16d98c389
sunghwki <52474291+swkim12345@users.noreply.github.com>
2022-12-27 13:04:21 +0900

    add git and except

---
+++ collect_price/__init__.py
@@ -0,0 +1,4 @@
+from . import kis_develop
+from . import stock_info
+from . import stock_price_procedure
+
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
-# import git
+import git
 import datetime
 import shutil
-import requests
 import os
 
 from collect_price import stock_info as info
@@ -12,17 +11,39 @@ if __name__=='__main__':
 	today_date = datetime.datetime.today().strftime('%Y_%m_%d')
 	dir_seperator = info._dir_seperator_check()
 	src = os.path.dirname(__file__) + dir_seperator + 'collect_price'
-	tar_dir = 'korea_min_stock_price'
-	tar_file = src + dir_seperator + today_date + '.tar.gz'
+	tar_dir = os.path.dirname(__file__) + dir_seperator + 'korea_min_stock_price'
+	tar_file = today_date + '.tar.gz'
+	url_file = 'url.yaml'
 
 	info.main()
 	price.main()
 
-	try:
-		if os.path.isfile(tar_file):
-			shutil.move(tar_file, tar_dir)
-		else:
-			print('not exists')
-	except:
-		print('not exists')
+	url = price._read_yaml(src + dir_seperator + 'conf', url_file)
 
+	try:
+		shutil.move(src + dir_seperator + tar_file, tar_dir)
+	except FileNotFoundError as e:
+		price._send_slack(url['slack_webhook_url'], f'File Not Found Error! \n Detail \n {e}')
+	except:
+		price._send_slack(url['slack_webhook_url'], 'Main Error! PLZ check log file.')
+
+	try:
+		repo = git.Repo(tar_dir)
+		index = repo.index
+		remote = repo.remote()
+	except:
+		print("error in init")
+	try:
+		index.add(tar_dir + dir_seperator + tar_file)
+
+	except:
+		print('Error in git error in add')
+	try:
+		index.commit(f'add {today_date}.tar.gz file')
+	except:
+		print("error in commit")
+	try:
+		remote.push()
+	except:
+		print("error in push")
+	price._send_slack(url['slack_webhook_url'], 'End collect price')
